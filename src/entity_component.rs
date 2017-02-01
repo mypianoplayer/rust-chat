29ce94d9abb2e45ed0fbf3c166faa733faa29a0d
myoshida <myoshida@aiming-inc.com>
2017-02-01 13:40:50 +0900

    progress

--- src/entity_component.rs
+++ src/entity_component.rs
@@ -1,7 +1,5 @@
-use std::borrow::Borrow;
 use std::cell::RefCell;
-use std::slice;
-use std::sync::Arc;
+use std::sync::{Arc,Weak};
 
 pub trait Component {
     fn update(&mut self);
@@ -13,6 +11,11 @@ pub struct Entity {
 }
 
 impl Entity {
+    pub fn new() -> Entity {
+        Entity {
+            comps: Vec::new()
+        }
+    }
     pub fn add_component(& mut self, comp : Arc<RefCell<Component>> ) {
         self.comps.push( comp );
     }
@@ -22,7 +25,7 @@ impl Entity {
 }
 
 pub struct System {
-    comps : Vec<Arc<RefCell<Component>>>,
+    comps : Vec<Weak<RefCell<Component>>>,
     objects : Vec<Box<Entity>>
 }
 
@@ -35,13 +38,15 @@ impl System {
     }
     pub fn add_entity(& mut self, entity:Entity) {
         for c in entity.components() {
-            self.comps.push(c.clone());
+            self.comps.push(Arc::downgrade(c));
         }
         self.objects.push(Box::new(entity));
     }
     pub fn update(&mut self) {
         for c in &self.comps {
-            c.borrow_mut().update();
+            if let Some(c) = c.upgrade() {
+                c.borrow_mut().update();
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ extern crate env_logger;
 extern crate rust_chat;
 
 use std::net::SocketAddr;
-use std::sync::mpsc;
+use std::sync::{Arc,Weak,mpsc};
+use std::cell::RefCell;
 use std::{thread, time};
 use mio_websocket::interface::*;
 use rust_chat::entity_component;
@@ -18,39 +19,74 @@ fn main() {
 
 }
 
+struct TextLabelComponent {
+    server: Weak<RefCell<WebSocket>>,
+    text : String,
+}
+
+impl TextLabelComponent {
+    fn new(text:String, sv: Weak<RefCell<WebSocket>>) -> TextLabelComponent {
+        TextLabelComponent {
+            text:text,
+            server:sv,
+        }
+    }
+}
+
+impl entity_component::Component for TextLabelComponent {
+    fn update(&mut self) {
+        if let Some(sv) = self.server.upgrade() {
+            sv.borrow_mut().send_all("update text".to_string());
+        }
+    }
+    fn typeid(&self) -> i32 {
+        1
+    }
+}
 
 struct Game1 {
-    system :entity_component::System,
-    server : WebSocket
-
+    system : Arc<RefCell<entity_component::System>>,
+    server : Arc<RefCell<WebSocket>>
 }
 
 impl Game1 {
 
     fn new(host_addr: &str) -> Game1 {
         Game1 {
-            system : entity_component::System::new(),
-            server : WebSocket::new(host_addr.parse::<SocketAddr>().unwrap())
+            system : Arc::new(RefCell::new(entity_component::System::new())),
+            server : Arc::new(RefCell::new(WebSocket::new(host_addr.parse::<SocketAddr>().unwrap()))),
         }
     }
 
-    fn onmessage(&mut self, msg: &String) {
+    fn onmessage(&self, msg: &String) {
         println!("{}", msg);
         if msg.eq("start") {
-
+            let mut obj = entity_component::Entity::new();
+            let cmp = TextLabelComponent::new("test".to_string(), Arc::downgrade(&self.server));
+            obj.add_component(Arc::new(RefCell::new(cmp)));
+            self.system.borrow_mut().add_entity(obj);
         }
     }
 
-    fn update(&mut self) {
-       for peer in self.server.get_connected().unwrap() {
-           let response = WebSocketEvent::TextMessage("tick".to_string());
-           self.server.send((peer, response));
-       }
-    }
+    // fn send_all(&mut self, msg: String) {
+    //     for peer in self.server.borrow_mut().get_connected().unwrap() {
+    //         let evt = WebSocketEvent::TextMessage(msg.clone());
+    //         self.server.borrow_mut().send((peer, evt));
+    //     }
+    // }
+
+    // fn update(&mut self) {
+    //     self.system.borrow_mut().update();
+    //    for peer in self.server.borrow_mut().get_connected().unwrap() {
+    //        let response = WebSocketEvent::TextMessage("tick".to_string());
+    //        self.server.borrow_mut().send((peer, response));
+    //    }
+    // }
 
     fn start(&mut self) {
         loop {
-            match self.server.try_next() {
+            let res = self.server.borrow_mut().try_next();
+            match res {
                 Ok(r) => {
                     match r {
                         (tok, WebSocketEvent::Connect) => {
@@ -64,7 +100,7 @@ impl Game1 {
                         (tok, WebSocketEvent::BinaryMessage(msg)) => {
                             println!("msg from {:?}", tok);
                             let response = WebSocketEvent::BinaryMessage(msg.clone());
-                            self.server.send((tok, response));
+                            self.server.borrow_mut().send((tok, response));
                         },
 
                         _ => {}
@@ -74,7 +110,7 @@ impl Game1 {
                     match e {
                         mpsc::TryRecvError::Empty => {
                             thread::sleep( time::Duration::from_millis(1000) );
-                            self.update();
+                            self.system.borrow_mut().update();
                         },
                         mpsc::TryRecvError::Disconnected => {
 
